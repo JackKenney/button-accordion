4db704ae2e2c86265fa1affe1b64de783b6a174f
Jack Kenney <jack@kenney.dev>
2025-04-28 00:27:27 -0400

    add new versions

--- src/diatonic-data.js
+++ src/diatonic-data.js
@@ -40,23 +40,12 @@ export const layouts = {
             ['B.5', 'E.6'],
         ],
     ],
-    'C#/D': [
+    'D/C#': [ // Joe Derrane's http://www.korbo.com/piedcrow/diagrams/irshDCshrp01.gif
         [], // blank for 2 row
         [
-            ['Bb.2', 'F.3'],
-            ['C.3', 'Ab.2'],
-            ['Eb.3', 'Db.3'],
-            ['Gb.3', 'F.3'],
-            ['Bb.4', 'Ab.3'],
-            ['C.4', 'Db.4'],
-            ['Eb.4', 'F.4'],
-            ['Gb.4', 'Ab.4'],
-            ['Bb.4', 'Db.5'],
-            ['C.5', 'F.5'],
-            ['Eb.5', 'Ab.5'],
-        ], [
-            ['C.3', 'Gb.3'],
-            ['Db.3', 'A.3'],
+            ['G.3', 'D.3'],
+            ['B.3', 'Gb.3'],
+            ['Db.4', 'A.3'],
             ['E.4', 'D.4'],
             ['G.4', 'Gb.4'],
             ['B.4', 'A.4'],
@@ -64,63 +53,76 @@ export const layouts = {
             ['E.5', 'Gb.5'],
             ['G.5', 'A.5'],
             ['B.5', 'D.6'],
-            ['Db.5', 'Gb.6'],
-        ],
-    ],
-    'V0': [ // semitone push/pull, only covers like 1 octave really well
-        [], // blank for 2 row
-        [
-            ['Ab.3', 'A.3'],
-            ['Bb.3', 'B.3'],
-            ['C.4', 'Db.4'],
-            ['D.4', 'Eb.4'],
-            ['E.4', 'F.4'],
-            ['Gb.4', 'G.4'],
-            ['Ab.4', 'A.4'],
-            ['Bb.4', 'B.4'],
-            ['C.5', 'Db.5'],
-            ['D.5', 'Eb.5'],
-            ['E.5', 'F.5'],
+            ['Db.6', 'Gb.6'],
         ], [
-            ['A.3', 'Bb.3'],
-            ['B.3', 'C.4'],
-            ['Db.4', 'D.4'],
-            ['Eb.4', 'E.4'],
-            ['F.4', 'Gb.4'],
-            ['G.4', 'Ab.4'],
-            ['A.4', 'Bb.4'],
-            ['B.4', 'C.5'],
-            ['Db.5', 'E.5'],
-            ['Eb.6', 'F.5'],
-        ],
-    ],
-    'V1': [ // whole steps B push, E push
-        [], // blank for 2 row
-        [
-            ['Db.3', 'B.3'],
-            ['F.3', 'Eb.3'],
-            ['A.3', 'G.3'],
-            ['Db.4', 'B.3'],
-            ['F.4', 'Eb.4'],
-            ['A.4', 'G.4'],
-            ['Db.5', 'B.4'],
-            ['F.5', 'Eb.5'],
-            ['A.5', 'G.5'],
-            ['Db.6', 'B.5'],
-            ['F.6', 'Eb.6'],
-        ], [
-            ['Gb.3', 'E.3'],
-            ['Bb.3', 'Ab.3'],
-            ['D.4', 'C.4'],
-            ['Gb.4', 'E.4'],
+            ['Bb.3', 'F.3'],
+            ['C.4', 'Ab.3'],
+            ['Eb.4', 'Db.4'],
+            ['Gb.4', 'F.4'],
             ['Bb.4', 'Ab.4'],
-            ['D.5', 'C.5'],
-            ['Gb.5', 'E.5'],
-            ['Bb.5', 'Ab.5'],
-            ['D.6', 'C.6'],
-            ['Gb.6', 'E.6'],
+            ['C.5', 'Db.5'],
+            ['Eb.5', 'F.5'],
+            ['Gb.4', 'Ab.5'],
+            ['Bb.5', 'Db.6'],
+            ['C.6', 'F.6'],
         ],
     ],
+    //// V0 is semitone push/pull, only covers like 1 octave really well
+    // 'V0': [ 
+    //     [], // blank for 2 row
+    //     [
+    //         ['Ab.3', 'A.3'],
+    //         ['Bb.3', 'B.3'],
+    //         ['C.4', 'Db.4'],
+    //         ['D.4', 'Eb.4'],
+    //         ['E.4', 'F.4'],
+    //         ['Gb.4', 'G.4'],
+    //         ['Ab.4', 'A.4'],
+    //         ['Bb.4', 'B.4'],
+    //         ['C.5', 'Db.5'],
+    //         ['D.5', 'Eb.5'],
+    //         ['E.5', 'F.5'],
+    //     ], [
+    //         ['A.3', 'Bb.3'],
+    //         ['B.3', 'C.4'],
+    //         ['Db.4', 'D.4'],
+    //         ['Eb.4', 'E.4'],
+    //         ['F.4', 'Gb.4'],
+    //         ['G.4', 'Ab.4'],
+    //         ['A.4', 'Bb.4'],
+    //         ['B.4', 'C.5'],
+    //         ['Db.5', 'E.5'],
+    //         ['Eb.6', 'F.5'],
+    //     ],
+    // ],
+    //// V1 uses whole steps up from push to pull, rows are a fifth apart
+    // 'V1': [ // whole steps B push, E push
+    //     [], // blank for 2 row
+    //     [
+    //         ['Db.3', 'B.3'],
+    //         ['F.3', 'Eb.3'],
+    //         ['A.3', 'G.3'],
+    //         ['Db.4', 'B.3'],
+    //         ['F.4', 'Eb.4'],
+    //         ['A.4', 'G.4'],
+    //         ['Db.5', 'B.4'],
+    //         ['F.5', 'Eb.5'],
+    //         ['A.5', 'G.5'],
+    //         ['Db.6', 'B.5'],
+    //         ['F.6', 'Eb.6'],
+    //     ], [
+    //         ['Gb.3', 'E.3'],
+    //         ['Bb.3', 'Ab.3'],
+    //         ['D.4', 'C.4'],
+    //         ['Gb.4', 'E.4'],
+    //         ['Bb.4', 'Ab.4'],
+    //         ['D.5', 'C.5'],
+    //         ['Gb.5', 'E.5'],
+    //         ['Bb.5', 'Ab.5'],
+    //         ['D.6', 'C.6'],
+    //         ['Gb.6', 'E.6'],
+    //     ],
+    // ],
     'V2': [ // whole steps C push, Eb push,
         [], // blank for 2 row
         [
@@ -202,6 +204,196 @@ export const layouts = {
             ['F.6', 'Eb.6'],
         ],
     ],
+    // 'V5': [ // whole steps rows major thirds push/pull
+    //     [], // blank for 2 row
+    //     [
+    //         ['C.3', 'E.3'],
+    //         ['D.3', 'Gb.3'],
+    //         ['E.3', 'Ab.3'],
+    //         ['Gb.4', 'Bb.3'],
+    //         ['Ab.4', 'C.4'],
+    //         ['Bb.4', 'D.4'],
+    //         ['C.5', 'E.5'],
+    //         ['D.5', 'Gb.5'],
+    //         ['E.5', 'Ab.5'],
+    //         ['Gb.6', 'Bb.6'],
+    //         ['Ab.6', 'C.6'],
+    //     ], [
+    //         ['G.3', 'B.3'],
+    //         ['A.3', 'Db.3'],
+    //         ['B.4', 'Eb.3'],
+    //         ['Db.4', 'F.4'],
+    //         ['Eb.4', 'G.4'],
+    //         ['F.5', 'A.4'],
+    //         ['G.5', 'B.5'],
+    //         ['A.5', 'Db.5'],
+    //         ['B.6', 'Eb.5'],
+    //         ['Db.6', 'F.6'],
+    //     ],
+    // ],
+    // 'V6': [
+    //     [], // blank for 2 row
+    //     [
+    //         ['Bb.2', 'C.3',],
+    //         ['D.3', 'E.3',],
+    //         ['Gb.3', 'Ab.3',],
+    //         ['Bb.3', 'C.4',],
+    //         ['D.4', 'E.4',],
+    //         ['Gb.4', 'Ab.4',],
+    //         ['Bb.4', 'C.5',],
+    //         ['D.5', 'E.5',],
+    //         ['Gb.5', 'Ab.5',],
+    //         ['Bb.5', 'C.6',],
+    //         ['D.6', 'E.6',],
+    //     ], [
+    //         ['Db.3', 'Eb.3'],
+    //         ['F.3', 'G.3'],
+    //         ['A.3', 'B.3'],
+    //         ['Db.4', 'Eb.4'],
+    //         ['F.4', 'G.4'],
+    //         ['A.4', 'B.4'],
+    //         ['Db.5', 'Eb.5'],
+    //         ['F.5', 'G.5'],
+    //         ['A.5', 'B.5'],
+    //         ['Db.6', 'Eb.6'],
+    //     ],
+    // ],
+    // 'V7': [ // whole step push, rows 4th apart
+    //     [], // blank for 2 row
+    //     [
+    //         ['Bb.2', 'C.3',],
+    //         ['D.3', 'E.3',],
+    //         ['Gb.3', 'Ab.3',],
+    //         ['Bb.3', 'C.4',],
+    //         ['D.4', 'E.4',],
+    //         ['Gb.4', 'Ab.4',],
+    //         ['Bb.4', 'C.5',],
+    //         ['D.5', 'E.5',],
+    //         ['Gb.5', 'Ab.5',],
+    //         ['Bb.5', 'C.6',],
+    //         ['D.6', 'E.6',],
+    //     ], [
+    //         ['Eb.3', 'F.3'],
+    //         ['G.3', 'A.3'],
+    //         ['B.3', 'Db.4'],
+    //         ['Eb.4', 'F.4'],
+    //         ['G.4', 'A.4'],
+    //         ['B.4', 'Db.5'],
+    //         ['Eb.5', 'F.5'],
+    //         ['G.5', 'A.5'],
+    //         ['B.5', 'Db.6'],
+    //         ['Eb.6', 'F.6'],
+    //     ],
+    // ],
+    'V8-B/C': [ // whole steps, B pull, C# push / C pull, D push
+        [], // blank for 2 row
+        [
+            ['B.2', 'Db.3'],
+            ['Eb.3', 'F.3'],
+            ['G.3', 'A.3'],
+            ['B.3', 'Db.4'],
+            ['Eb.4', 'F.4'],
+            ['G.4', 'A.4'],
+            ['B.4', 'Db.5'],
+            ['Eb.5', 'F.5'],
+            ['G.5', 'A.5'],
+            ['B.5', 'Db.6'],
+            ['Eb.6', 'F.6'],
+        ], [
+            ['C.3', 'D.3'],
+            ['E.3', 'Gb.3'],
+            ['Ab.3', 'Bb.3'],
+            ['C.4', 'D.4'],
+            ['E.4', 'Gb.4'],
+            ['Ab.4', 'Bb.4'],
+            ['C.5', 'D.5'],
+            ['E.5', 'Gb.5'],
+            ['Ab.5', 'Bb.5'],
+            ['C.6', 'D.6'],
+        ],
+    ],
+    'V8-C/C#': [ // whole steps, C pull, D push
+        [], // blank for 2 row
+        [
+            ['C.3', 'D.3'],
+            ['E.3', 'Gb.3'],
+            ['Ab.3', 'Bb.3'],
+            ['C.4', 'D.4'],
+            ['E.4', 'Gb.4'],
+            ['Ab.4', 'Bb.4'],
+            ['C.5', 'D.5'],
+            ['E.5', 'Gb.5'],
+            ['Ab.5', 'Bb.5'],
+            ['C.6', 'D.6'],
+            ['E.6', 'Gb.6'],
+        ], [
+            ['Db.3', 'Eb.3'],
+            ['F.3', 'G.3'],
+            ['A.3', 'B.3'],
+            ['Db.4', 'Eb.4'],
+            ['F.4', 'G.4'],
+            ['A.4', 'B.4'],
+            ['Db.5', 'Eb.5'],
+            ['F.5', 'G.5'],
+            ['A.5', 'B.5'],
+            ['Db.6', 'Eb.6'],
+        ],
+    ],
+    'V8-D/C#': [ // whole steps, D pull, E push / C# pull, D# push
+        [], // blank for 2 row
+        [
+            ['D.3', 'E.3'],
+            ['Gb.3', 'Ab.3'],
+            ['Bb.3', 'C.4'],
+            ['D.4', 'E.4'],
+            ['Gb.4', 'Ab.4'],
+            ['Bb.4', 'C.5'],
+            ['D.5', 'E.5'],
+            ['Gb.5', 'Ab.5'],
+            ['Bb.5', 'C.6'],
+            ['D.6', 'E.6'],
+            ['Gb.6', 'Ab.6'],
+        ], [
+            ['F.3', 'G.3'],
+            ['A.3', 'B.3'],
+            ['Db.4', 'Eb.4'],
+            ['F.4', 'G.4'],
+            ['A.4', 'B.4'],
+            ['Db.5', 'Eb.5'],
+            ['F.5', 'G.5'],
+            ['A.5', 'B.5'],
+            ['Db.6', 'Eb.6'],
+            ['F.6', 'G.6'],
+        ],
+    ],
+    'V8-D/D#': [ // whole steps, D pull, E push
+        [], // blank for 2 row
+        [
+            ['D.3', 'E.3'],
+            ['Gb.3', 'Ab.3'],
+            ['Bb.3', 'C.4'],
+            ['D.4', 'E.4'],
+            ['Gb.4', 'Ab.4'],
+            ['Bb.4', 'C.5'],
+            ['D.5', 'E.5'],
+            ['Gb.5', 'Ab.5'],
+            ['Bb.5', 'C.6'],
+            ['D.6', 'E.6'],
+            ['Gb.6', 'Ab.'],
+        ], [
+            ['Eb.3', 'F.3'],
+            ['G.3', 'A.3'],
+            ['B.3', 'Db.4'],
+            ['Eb.4', 'F.4'],
+            ['G.4', 'A.4'],
+            ['B.4', 'Db.5'],
+            ['Eb.5', 'F.5'],
+            ['G.5', 'A.5'],
+            ['B.5', 'Db.6'],
+            ['Eb.6', 'F.6'],
+        ],
+    ],
+    // V9 flips push and pull of V8 to more align with tradition
 }
 
 export const bassLayouts = {
@@ -217,7 +409,7 @@ export const bassLayouts = {
         [['F.3', 'A.3', 'C.4'], ['C.3', 'E.3', 'G.3']],
         [['F.2'], ['C.2']],
     ]],
-    'C#/D': [[
+    'D/C#': [[
         [['E.3', 'Ab.3', 'B.4'], ['A.3', 'Db.3', 'E.4']],
         [['E.2'], ['A.2']],
         [['A.3', 'Db.3', 'E.4'], ['D.3', 'Gb.3', 'A.3']],
@@ -247,19 +439,37 @@ export const bassLayouts = {
 export const getLayout = (tuning) => {
     const arrangement = layouts[tuning]
     const [one, two, three] = getRows(arrangement)
-
     return { one, two, three }
+}
+
+export const getNextNote = (note, octave) => {
+    const i = notes.indexOf(note)
+    const j = (i + 1) % notes.length
+    const newOctave = parseInt(octave) + ((j == 0) ? 1 : 0) // wrapped around?
+    const newNote = notes[j]
+    return { note: newNote, octave: newOctave }
 }
 
 export const getRows = (arrangement) => arrangement ? arrangement.map((row, r) => (
     row.map((button, b) => {
         const [pull, push] = button;
-        const [pull_note, pull_octave] = pull.split('.')
-        const [push_note, push_octave] = push.split('.')
-
+        const [pullNote, pullOctave] = pull.split('.')
+        const [pushNote, pushOctave] = push.split('.')
+        const { note: pullNoteNext, octave: pullOctaveNext } = getNextNote(pullNote, pullOctave)
+        const { note: pushNoteNext, octave: pushOctaveNext } = getNextNote(pushNote, pushOctave)
         return [
-            { id: `${r + 1}-${b + 1}-pull`, name: pull_note, frequency: tone[pull_note][parseInt(pull_octave)] },
-            { id: `${r + 1}-${b + 1}-push`, name: push_note, frequency: tone[push_note][parseInt(push_octave)] },
+            {
+                name: pullNote,
+                id: `${r + 1}-${b + 1}-pull`,
+                frequency: tone[pullNote][parseInt(pullOctave)],
+                nextFrequency: tone[pullNoteNext][parseInt(pullOctaveNext)]
+            },
+            {
+                name: pushNote,
+                id: `${r + 1}-${b + 1}-push`,
+                frequency: tone[pushNote][parseInt(pushOctave)],
+                nextFrequency: tone[pushNoteNext][parseInt(pushOctaveNext)]
+            },
         ]
     }).flat()
 )) : [[], [], []]
@@ -267,7 +477,6 @@ export const getRows = (arrangement) => arrangement ? arrangement.map((row, r) =
 export const getBassLayout = (tuning) => {
     const arrangement = bassLayouts[tuning]
     const [one, two, three] = getBassRows(arrangement)
-
     return { one, two, three }
 }
 
@@ -297,12 +506,16 @@ const findScale = (tuning, note, intervals) => {
     const original_idx = notes.indexOf(note)
     let curr = original_idx
     let lastRow = 0;
-    let octave = 4
-    const nextOctave = octave + 1
+    const firstOctave = 4
+    const secondOctave = 5
+    let octave = firstOctave
     intervals.forEach((interval) => {
         curr = curr + interval
-        if (notes.length <= curr) {
-            octave = nextOctave
+        if (curr < notes.length) {
+            octave = firstOctave
+        }
+        else if (notes.length <= curr) {
+            octave = secondOctave
         }
         const find = notes[curr % notes.length] // find next note name
         let found = false;
@@ -368,12 +581,11 @@ export const bassKeyMap = {
 export const rowMap = { 2: 'two', 3: 'three' }
 export const bassRowMap = { 1: 'one', 2: 'two' }
 export const rowTones = {
-    'V4': { one: '', two: 'outer', three: 'inner' },
-    'V3': { one: '', two: 'outer', three: 'inner' },
-    'V2': { one: '', two: 'outer', three: 'inner' },
-    'V1': { one: '', two: 'outer', three: 'inner' },
-    'V0': { one: '', two: 'outer', three: 'inner' },
-    'C#/D': { one: '', two: 'C#', three: 'D' },
+    ...Object.keys(layouts).reduce((c, k) => {
+        c[k] = { one: '', two: 'outer', three: 'inner' };
+        return c;
+    }, {}),
+    'D/C#': { one: '', two: 'D', three: 'C#' },
     'B/C': { one: '', two: 'B', three: 'C' },
 }
 
